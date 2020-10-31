5545a5b7e573d1359f93a1b103a0a7e8f5cd8259
ViviJiSe <viviam.jimenez@ulead.ac.cr>
2020-10-30 22:09:22 -0600

    Exercise1

--- src/NaturalNumbers_Exercise1.java
+++ src/NaturalNumbers_Exercise1.java
@@ -1,2 +1,20 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class NaturalNumbers_Exercise1 {
+
+    private static PrintStream out = new PrintStream(System.out);
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int counter=0;
+
+        while(counter <=10){
+            out.println(counter);
+            counter++;
+
+        }
+    }
+
+
 }
