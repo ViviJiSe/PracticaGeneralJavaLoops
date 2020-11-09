21553530ee0b958fe6b2ad2de78a20fd4f766d1f
ViviJiSe <viviam.jimenez@ulead.ac.cr>
2020-11-08 23:59:17 -0600

    Exercie 9

---
+++ src/NegativeNumberExercise9.java
@@ -0,0 +1,9 @@
+public class NegativeNumberExercise9 {
+
+    public static void main(String[] args) {
+
+        for (int counter = -10; counter <= -1; ++counter) {
+            System.out.println(counter);
+        }
+    }
+}
